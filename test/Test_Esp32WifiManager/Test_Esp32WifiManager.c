ec50a5b6908e3b61fc0ccf9da8ae27d433dc1db7
Matthew Ayestaran <matthew@MainPC.lan>
2025-10-25 10:43:37 +0100

    start of day place to revert back to

--- test/Test_Esp32WifiManager/Test_Esp32WifiManager.c
+++ test/Test_Esp32WifiManager/Test_Esp32WifiManager.c
@@ -2,10 +2,9 @@
  * description: number of tests for the esp32wifimanager header file to confirm
  * functionality created by: matthew ayestaran date: 19-10-2025
  * */
-/*
+
 #ifndef Test_Esp32WifiManager_c
 
-#include <fff.h>
 #include <unity.h>
 
 FAKE_VALUE_FUNC(esp_err_t, esp_wifi_connect);
@@ -16,5 +15,4 @@ FAKE_VALUE_FUNC(EventBits_t, xEventGroupClearBits, EventGroupHandle_t,
 
 #include "esp32wifimanager.h"
 
-#endif  //fndef Test_Esp32WifiManager.c"
-*/
+#endif // fndef Test_Esp32WifiManager.c"
